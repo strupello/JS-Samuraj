b7a004af5f5d906bb2008712361fc92dc8586ffb
strupello <irek1992@gmail.com>
2025-12-14 22:49:38 +0100

    Add lesson 1 files

---
+++ main.js
@@ -0,0 +1,132 @@
+/* console.log(3 * 9);
+
+2 + 2;
+let result = 2 + 2;
+
+
+let prymitiveType1 = 20;
+let prymitiveType2 = prymitiveType1;
+prymitiveType1 = 40;
+console.log(prymitiveType2);
+
+
+let referenceValue1 = [1, 2, 3];
+let referenceValue2 = referenceValue1;
+referenceValue1[3] = 4;
+console.log(referenceValue2);
+
+
+let firstLetterUserName;
+firstLetterUserName = 'JP';
+
+
+let nickName = "Bzyku";
+let showFullName = function (){
+    console.log(nickName + firstLetterUserName);
+} */
+
+/* let name, age, address;
+let position = [200, 100], score = null, time = 20.2, ipAddress;
+const firstUser = "Ania", secondUser = "Tomasz"; */
+
+/* let example1 = 333;
+example1 = example1 + 333;
+console.log(example1);
+
+const example2 = 333;
+console.log(example2);
+
+let example3 = example2;
+example3 = example3 * 3;
+console.log(example3); */
+
+/* let arrayExample1 = [1];
+arrayExample1 = [1, 2];
+
+const arrayExample2 = [1];
+// arrayExample2 = [1];
+arrayExample2[1] = 2, 3;
+
+let letVariable = 1;
+console.log(letVariable);
+ */
+
+/* let result;
+result = 1;
+result++; */
+
+// let position = 2;
+// inkrementacja
+// position++;
+// dekrementacja
+// position--;
+// preinkrementacja
+// ++position;
+// predekrementacja
+// --position;
+// przypisanie
+
+// position += 10;
+// position -= 16.5;
+// position *= 4;
+// position /= 2;
+
+/* console.log(position);
+
+let newPosition = 5;
+newPosition *= position;
+console.log(newPosition); */
+
+// Modulo reszta z dzielenia
+// 10 % 9;
+// 20 % 4;
+
+// Potęgowanie
+// 2 ** 5;
+
+/* Operator Logiczny */
+// Operator porównania (większy/mniejszy)
+
+// 20 > 40;
+// "teskst".length > 3;
+// 20 <= 20;
+
+//operator porówania (czy są równe)
+
+// "tekst" == "tekst";
+// "tekst" == "tekst1";
+
+// "2" == 2;  dokonuje konwersji typów
+// "2" === 2;  bez konwersji typów, a więc porównuje też typy
+
+// operator różne od
+// 100 != 101;
+// 100 != 100;
+// 100 !== 100;
+
+// logiczne zaprzeczenie (negacja) zamienia na typ boolean i odwraca
+// !("coś");
+// !!(2 - 2); // podówjna negacja
+// !!200;
+
+// Suma logiczna  (logiczne "lub")
+// true || false;
+// false || true;
+// true || true;
+// false || false;
+// (true || false);
+// (null || 200);
+
+// Iloczyn logcziny (logiczne "i")
+
+// true && false;
+// 200 && 0;
+// "1" && "2";
+// "1" && "";
+// (20 > 10 && 10 < 20);
+
+// KOLEJNOŚĆ OPERATORÓW
+
+// 2 + 3 * 5  = 17
+//(2 + 3) * 5 = 25;
+// "2" + 3 * 5 = 215;
