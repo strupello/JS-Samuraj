b66e7671b8df72a48042572e8048c350be5226db
strupello <irek1992@gmail.com>
2025-12-15 23:36:58 +0100

    lesson 17-18

--- main.js
+++ main.js
@@ -130,3 +130,54 @@ console.log(newPosition); */
 // 2 + 3 * 5  = 17
 //(2 + 3) * 5 = 25;
 // "2" + 3 * 5 = 215;
+
+// Literały:
+/* "jakiś tekst"; 1222; true; null; undefined; - Każdy typ prosty (poza Symbolem) można stworzyć za pomocą literału. Literał to zestaw znaków które zostaną rozpoznane przez interpreter. */
+// literał łańcucha, literał liczby
+// na przyszłość: literał obiekut '{}', literał tablic '[]';
+
+// Typy proste nie śa obiektami, to jednak możemy na nich (number, string, boolean) wykonywać metody i właściwości, które to są domeną obiektów
+
+// właściwość - informacja o stanie (wartości np:długość)
+// metoda - działanai (na wartości), które najcześciej zwraca nową wartość w oparciu o działanie na wartości.
+
+// Własciowść
+
+// let nickName = "Bzyku";
+// nickName.length;
+// nickName[2];
+
+// Metody ( nie dokonują zmian, tylko zwracają). Metody muszą być wywowałane (()), bez wywołania zobaczymy tylko czym są a nie ich działanie.
+// metoda(argument)
+// const firstText = "przykładowy tekst dla meteody";
+// let score = 12.56;
+
+// firstText.toUpperCase;
+// firstText.toUpperCase();
+// score.toFixed();
+// firstText.indexOf('p');
+
+
+// Dlatego te nowe wartości gdzieś powinniśmy przypisać
+
+// const newTxt = firstText.toUpperCase();
+// const fixedNumber = score.toFixed();
+
+// Typy proste nie mają właściwości i metod. Jak to się więc dzieje, że możemy pobrać ich właściwości czy wykonać na nich metody (jak na obiektach)?
+// 1 - tworzymy zmienną i wartość string
+const name = "Janek";
+name.length; //właściwość której nie ma w typie prostym, i teraz zaczyna pracować silnik, który robi sobie mniej więcej coś takiego jak ponizej by pobrać wartość tej właściwości
+// 2- SILNIK TWORZY OBIEKT NA TYM ETAPIE (nazwa zmiennejj tylko dla przykładu)
+//Tworzy obiekt String np.
+// const nameTemporary = new String(name);
+//nameTemporary.length
+// 3 - Zwraca 5 (bo taką ma długość "Janek") i usuwa obiekt String (w przykłądzie znajduje się w nameTemporary)
+
+/* Reasumując: Jeżeli wywołujemy metody, czy chcemy dostać sie do zawartości własciwości, to w tle tworzony jest tymaczowy obiekt danego typu, który "opakowuje" nasz typ prosty na czas dotarcia do właściwości czy wywowłania metody.*/
+
+// Możemy też tworzyć takie obiekty sami (abstachując już od opakowania typów prostych). Ale bardzo rzadko ma to sens, ponieważ takie obiekty mają częstoi inne zachowania niż typy proste np. każdy obiekt zwraca np. true, a operator typeof zwróci "object", a nie rodzaj typu prostego. problemy pojawią się także przy operatorze porównania "==="). Tworzenie obektu (String, Number, Boolean) jest trudniejsze niż prosty literał.
+const userName = new String("Jagienka");
+const userID = new Number(2);
+const famale = new Boolean(true);
+
+//Na przyszłość: String, Number i Boolean to przykłady konstrukturów czyli funkcji, które są używane do tworzenie instancji (niezależnej kopii) obiektów. Operator "new" tworzy nowy obiekt. Na tym etapie nie masz jeszcze szans tego zrozumieć, to zaawasnowany koncept programowania w JS, ale kiedy dojdziemy do programowania obiektowego będę to dobrze wyjaśniał i zrozumiesz ten mechanizm.
