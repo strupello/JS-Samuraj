a6f8560870ab1e1e731746d4cdc7767f1d7d533c
strupello <irek1992@gmail.com>
2025-12-17 23:11:00 +0100

    finished lesson about number

--- number-pratice.js
+++ number-pratice.js
@@ -1,5 +1,6 @@
-const praticeNumber = 421.576
-
+const praticeNumber = 421.576;
+decimalExaple = 12.22;
+integreExample = 222;
 // praticeNumber.toFixed();
 // praticeNumber.toFixed(1);
 
@@ -12,5 +13,31 @@ const praticeNumber = 421.576
 // 421.toString(16); - źle
 // 421..toString(16); - dobrze
 
-
 // praticeNumber.toExponential(); - zmiana na notacje naukową
+
+// integer numbers examples - liczby całkowite, decimal numbers example - liczby dziesiętne
+
+// Number.isInteger(decimalExaple);
+// Number.isInteger(12.22)
+// Number.isInteger(integreExample);
+// Number.isInteger(222)
+
+// sprawdzanmie czyu dana liczba jest skończona. (prawidłowa)
+// Number.isFinite(2);
+// Number.isFinite(NaN);
+// Number.isFinite(Infinity);
+// Number.isFinite(3e4000); // false, ponieważ, maksymalna liczba to 1e308, najmniejsza licza -1e308
+// Number.isFinite(3e300); // true
+
+// typeof NaN;
+
+// Działania arytrmetyczne - przykłady
+
+// Reszta z dzielenia - modulo
+
+// console.log(20 % 2); 0
+// console.log(20 % 3); 2
+// console.log(20 % 21); 20
+
+// Potęgowanie
+// 2 ** 8
