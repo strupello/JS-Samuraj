63fa88caacf61d60b8805e5be0959f04a8ef6bc2
strupello <irek1992@gmail.com>
2025-12-22 23:17:11 +0100

    add lesson about string/boolean

---
+++ string-boolean.js
@@ -0,0 +1,11 @@
+// const userName1 = "STRUP";
+// const userName2 = "GRUBY";
+// const userName3 = "TRENDAFIX TO JEST GOŚĆ"
+
+// const numberOfLetter = userName1[4];
+// console.log(numberOfLetter);
+
+// const textSuper = `teskt jakiś tam "chuj CI w dupe" z i 'apostorfem'`
+// console.log(textSuper);
+
+// userName1.lenght
