c10469a568091867d91bdaf10fece97c42bae74a
strupello <irek1992@gmail.com>
2026-01-04 18:47:23 +0100

    Finished lesson about if/switch/operative

---
+++ if-pratice.js
@@ -0,0 +1,76 @@
+// const itIsOkHere = 14;
+
+// if (itIsOkHere) {
+//     console.log("Wracamy do nauki!");
+//     alert("Na pełnej pizdzie!")
+// }
+
+// const firstName = "Irek";
+
+// if (firstName) {
+//     console.log("Witaj " + firstName + '!');
+// }
+
+// if (firstName !== "") {
+//     console.log("Witaj " + firstName);
+// }
+
+// Instrukcja Warunkowa:
+
+// const hasMoney = 1;
+// const moreThan17gold = 0;
+// const ifHeKnowBoss = 1;
+
+// if (hasMoney && moreThan17gold || ifHeKnowBoss) {
+//     console.log("You have new sword");
+// } else {
+//     console.log("Sorry, you don't have enough gold!");
+// }
+
+// Klauza else if
+
+// const gold = 11;
+// if (gold) {
+//   console.log("You have one sword with 0 skill.");
+// } else if (gold <= 20) {
+//   console.log("You have one sword with 1 skill.");
+// } else if (gold <= 50) {
+//   console.log("You have one sword with 2 skill's.");
+// } else if (gold <= 100) {
+//   console.log("You have sword with 3 skill's.");
+// } else {
+//     console.log("You have sword with 3 skill's and ultimate option.");
+// }
+
+// INSTRUKCJA WARUNKOWA SWITCH
+
+// const threeDefaultColors = "yellow";
+
+// switch (threeDefaultColors) {
+//   case "red":
+//     console.log("it is red");
+//     break;
+//   case "blue":
+//     console.log("it is blue");
+//     break;
+//   case "yellow":
+//     console.log("it is yellow");
+//     break;
+//   default:
+//     console.log("it is another color");
+//     break;
+// }
+
+
+// const firstName = "Irek"
+// 20 == "20" && firstName ? console.log("wykonuje się, bo prawda " + "Twój Pan to " + firstName) : console.log("wykonuje się bo warunek nie jest prawdziwy " + "Twój Pan to " + firstName);
+
+// const score = 135
+// score > 120 ? console.log("prawda") : console.log("fałsz");
+
+// console.log(5 <= 20 ? "chujCiWDupe" : "nawetDwa");
+
+
+let expDota = 0;
+let killedEnemyHero = 17;
+expDota += killedEnemyHero > 15 ? "przeChuj jesteśszmaciarz" : 0;
