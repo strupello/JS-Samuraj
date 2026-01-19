bf76b1783cf43b192141499dfc9ea08fc2ac65d7
strupello <irek1992@gmail.com>
2026-01-19 23:39:59 +0100

    Still pratice this same, after break

--- pratice-with-chatgpt.js
+++ pratice-with-chatgpt.js
@@ -1,38 +1,93 @@
-const userName = "Irek;"
-const age = 34;
-const isAdmin = false;
+// const userName = "Irek;"
+// const age = 34;
+// const isAdmin = false;
 
-if ( age >= 18 ) {
-    console.log("No ten prze chuj jest pełnoletni");
+// if ( age >= 18 ) {
+//     console.log("No ten prze chuj jest pełnoletni");
+// } else {
+//     console.log("A ta małoletnia jebana pała.");
+// }
+
+// const scores = [12, 55, 8, 101, 33];
+
+// if ( scores[0] >= scores[scores.length - 1] ) {
+//     console.log("pierwszy wygrywa");
+// } else {
+//     console.log("ostatni wygrywa");
+// }
+
+// const text = "dota lol cs valorant".split(' ');
+// console.log(text[0]);
+// console.log(text[text.length - 1]);
+// console.log(text.includes("cs"));
+
+// const hasGold = false;
+// const hasSword = false;
+
+// if (hasGold && hasSword) {
+//     console.log("ready");
+// } else if  (hasGold || hasSword) {
+//     console.log("almost");
+// }
+// else { console.log("nope");}
+
+// const kills = [0, 3, 15, 22, 1, 18];
+// let proCount = 0;
+// for (let i = 0; i < kills.length; i++) {
+//   if (kills[i] >= 15) {
+//     proCount = proCount + 1;
+//   }
+// }
+// if (proCount >= 2) {
+//     console.log("PRO");
+// } else {
+//     console.log("CASUAL");
+// }
+// console.log(proCount);
+
+const temp = 16;
+
+if (temp < 0) {
+  console.log("Mróz");
+} else if (temp <= 15) {
+  console.log("zimno");
 } else {
-    console.log("A ta małoletnia jebana pała.");
+  console.log("ciepło");
 }
 
-const scores = [12, 55, 8, 101, 33];
+const prices = [12, 55, 8, 101, 33];
 
-if ( scores[0] >= scores[scores.length - 1] ) {
-    console.log("pierwszy wygrywa");
+if (prices[0] >= prices[prices.length - 1]) {
+  console.log("first cheaper");
 } else {
-    console.log("ostatni wygrywa");
+  console.log("last cheaper");
 }
 
-
-const kills = [0, 3, 15, 22, 1, 18];
-for (let proCount = 0; )
-
-
-
-const text = "dota lol cs valorant".split(' ');
-console.log(text[0]);
-console.log(text[text.length - 1]);
-console.log(text.includes("cs"));
-
-
-const hasGold = true;
-const hasSword = true;
-
-if (hasGold && hasSword) {
-    console.log("gotowy do walki");
+const levels = [1, 4, 7, 2, 9, 10, 3];
+let howManyLevels = 0;
+for (let i = 0; i < levels.length; i++) {
+  if (levels[i] >= 7) howManyLevels = howManyLevels + 1;
+}
+if (howManyLevels >= 3) {
+  console.log("HIGH LEVEL");
 } else {
-    console.log("prawie gotowy");
+  console.log("LOW LEVEL");
+}
+console.log(howManyLevels);
+
+const sentence = "js jest trudny ale wchodzi".split(" ");
+const quantitySentence = sentence.length;
+const checkTextOnSentence = sentence.includes("trudny");
+console.log(quantitySentence);
+console.log(checkTextOnSentence);
+
+const hasKey = true;
+const hasTicket = false;
+
+if (hasKey && hasTicket) {
+  console.log("Wchodzisz");
+} else if (hasKey || hasTicket) {
+  console.log("prawie");
+} else {
+  console.log("nie wchodzisz");
 }
